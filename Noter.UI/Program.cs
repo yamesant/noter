8bd95abd9d7df7fc2e68a0958e29c0960ba8c74c
yamesant <165981998+yamesant@users.noreply.github.com>
2025-02-25 14:18:47 +1000

    Apply scaffolding

--- Noter.UI/Program.cs
+++ Noter.UI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Noter.Domain;
+using Microsoft.AspNetCore.Identity;
+using Noter.UI.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,8 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DataContextConnection"));
 });
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DataContext>();
 
 var app = builder.Build();
 
